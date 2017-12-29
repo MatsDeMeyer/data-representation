8e3d2f689257fbe5e4cfe6c66bcf8886ce64f4e6
Mats De Meyer <matsdemeyer@gmail.com>
2017-12-30 00:05:47 +0100

    Added stop word removal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv_helper
 from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
 
+#PART 1 Pre-Processing
 #load the csv file
 tweets = csv_helper.CSVHelper.load_csv("Dataset\Tweets_2016London.csv")
 print(tweets)
@@ -10,3 +12,19 @@ tknzr = TweetTokenizer()
 #make list of tokens instead of list of tweets
 tokenizedTweets = [tknzr.tokenize(i) for i in tweets]
 print(tokenizedTweets)
+
+#remove stop words (to get these stopwords open python console and run:
+#import nltk
+#nltk.download("stopwords")
+stopwords = stopwords.words('english')
+print(stopwords)
+#remove every word from tokenized tweets which is in stopwords (keep the rest)
+
+
+#works, but inefficient
+filteredTweets = tokenizedTweets
+for tweet in filteredTweets:
+    for word in tweet:
+        if word in stopwords:
+            tweet.remove(word)
+print(filteredTweets)
