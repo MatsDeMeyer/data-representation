0620afba8c1c00458f79a6f97ec9e0e0c773c15d
Mats De Meyer <matsdemeyer@gmail.com>
2017-12-30 15:08:49 +0100

    poging 1 tot tf-idf

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import csv_helper
 from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+from scipy import sparse
+import numpy as np
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-#PART 1 Pre-Processing
+#PART 1: Pre-Processing
 #load the csv file
 tweets = csv_helper.CSVHelper.load_csv("Dataset\Tweets_2016London.csv")
 
@@ -25,3 +29,11 @@ print("Filtered Tweets: ", filteredTweets)
 st = SnowballStemmer("english")
 stemmedTweets = [[st.stem(word) for word in tweet if word not in stopwords] for tweet in tokenizedTweets]
 print("Stemmed Tweets: ", stemmedTweets)
+
+#PART 2: Noise Removal
+#TF-IDF (werkt nog niet)
+#http://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.TfidfVectorizer.html
+#http://scikit-learn.org/stable/auto_examples/text/document_clustering.html#sphx-glr-auto-examples-text-document-clustering-py
+
+#vectorizer = TfidfVectorizer(max_df=0.5, max_features=10000, min_df=2, stop_words='english', use_idf=True)
+#result = vectorizer.fit_transform(stemmedTweets)
